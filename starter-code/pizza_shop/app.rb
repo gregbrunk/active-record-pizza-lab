5c016feab29d715c1cdd455103aba50232cd49ae
Greg Brunk <greg.brunk@gmail.com>
2016-07-14 15:36:43 -0600

    Update route, finished homework!

--- starter-code/pizza_shop/app.rb
+++ starter-code/pizza_shop/app.rb
@@ -3,11 +3,13 @@ require "./models/pizza.rb"
 
 class PizzaShop < Sinatra::Base
 	# General route actions
+	# Get Route
 	get '/pizzas' do
 		@all_pizzas = Pizza.all
 		@all_pizzas.to_json
 	end
 
+	# Create Route
 	post '/pizzas' do
 		request.body.rewind
         new_pizza = request.body.read
@@ -15,6 +17,16 @@ class PizzaShop < Sinatra::Base
 		Pizza.create(parsed_pizza)
 	end
 
+	# Update Route
+	put '/pizzas/:id' do
+		new_pizza = request.body.read
+		parsed_new_pizza = JSON.parse(new_pizza)
+		@updating_pizza = Pizza.find(params[:id])
+		@updating_pizza.update_attributes(parsed_new_pizza)
+		redirect('/pizzas')
+	end
+
+	# Delete Route
 	delete '/pizzas/:id' do
 		@deleted_pizza = Pizza.find(params[:id])
 		@deleted_pizza.destroy
